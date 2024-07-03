6fbe198abfc7db605e88cb80e63e9e021515d296
zhuuua <z2142439320@outlook.com>
2024-07-03 21:37:07 +0800

    Add files via upload

---
+++ text1.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	printf("比特\n");
+//	return 0;
+//}
+//int main()
+//{
+//	//字符类型
+//	char ch = 'a';
+//	//整型
+//	int age = 10;
+//	//短整型
+//	short int num = 10;
+//	//单精度浮点型
+//	float weight = 55.5;
+//	//双精度浮点型
+//	double d = 0.0;
+//	return 0;
+//}
+//int main()
+//{
+//	//printf("hehe\n");
+//	//printf("比特");
+//	//printf("%d/n", 100);
+//	//sizeof-关键字-操作符-计算类型或变量所占的空间大小
+//	printf("%d\n", sizeof(char));
+//	return 0;
+//
+//int main()
+//{
+//	int age = 10;
+//	double weight = 57.3;
+//
+//	age = age + 1;
+//	weight = weight - 5;
+//	printf("%d\n", age);
+//	printf("%lf\n", weight);
+//	return 0;
+//}
+//%d-整型
+//%lf-double
+//%f-float
+
+//int main()
+//{
+//	int a = 10;
+//	return 0;
+//	printf("%d\n", a);
+//}
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum=%d\n", sum);
+	return 0;
+}
+
+
+
+  
